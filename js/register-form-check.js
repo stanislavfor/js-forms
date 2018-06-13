ce7423d43b20026f6563c227a06e1c1ff0c8f752
stanislavfor <stasposter@narod.ru>
2018-06-14 00:57:11 +0300

    Fixed password validation commit

--- js/register-form-check.js
+++ js/register-form-check.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
 	var loginFormCheck = (function(){
 
 		// Переменные модуля
-		var _loginForm = $('#login-add-form');
+		var _loginForm = $('#register-add-form');
 		var _inputEmailName = $('#inputEmailName');
 		var _errorMail = $('#errorMail');
 		var	_patternEmail = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
 		var _errorMailFormat = $('#errorMailFormat')
-		var _errorDescription = $('#errorDescription');
+		var _errorDescriptionBusy = $('#errorDescriptionBusy');
 		var _errorDescriptionDesc = $('#errorDescriptionDesc');
 		var	_inputPasswordName = $('[name="password"]');
 		var _errorPassword = $('#errorPassword');
@@ -34,39 +34,37 @@ $(document).ready(function(){
 				var _emailName = _inputEmailName.val().trim().toLowerCase();
 				var _passwordName = _inputPasswordName.val().trim();
 
-				 	// Проверка email и пароля
-					if ( _inputEmailName.val() == ''	) { 
-							_errorMail.fadeIn(1000);
-					} else {
+				// Проверка email
+				if ( _inputEmailName.val() == ''	) {
+						_errorMail.fadeIn(1000);
+				} else {
 						if ( _patternEmail.test( _emailName )) {
 								console.log('Hello from  _patternEmail.test');
-								if ( _emailName == "mail@mail.com" && _passwordName == "123" ) {
-												_loginForm.unbind('submit').submit();
-											} else {
-											_errorDescription.fadeIn(1000);
-											_errorDescriptionDesc.fadeIn(1000);
-											event.preventDefault();	
-											}	
-									} else {
-										_errorMailFormat.fadeIn(1000);
-									}
-								}	if ( _inputPasswordName.val() == ''	) {
+								if ( _emailName == "mail@mail.com") {
+									_errorDescriptionBusy.fadeIn(1000);
+									_errorDescriptionDesc.fadeIn(1000);
+									event.preventDefault();
+									// Проверка пароля
+								} else if ( _inputPasswordName.val() == ''	) { 
 										_errorPassword.fadeIn(1000);
 										event.preventDefault();
-								} else {
-										_errorPassword.fadeOut();			
-							}
+										} else {
+										_loginForm.unbind('submit').submit();
+										}
+						} else {
+							_errorMailFormat.fadeIn(1000)
+						}
+					}
 
-
-					$('#inputEmailName').on('focus', function(){
-								_error.fadeOut();
-								_errorDescriptionDesc.fadeOut();												
-							});	
-					$('#inputPasswordName').on('focus', function(){
-								_error.fadeOut();
-								_errorDescriptionDesc.fadeOut();	
-							});	
-				}
+				$('#inputEmailName').on('focus', function(){
+							_error.fadeOut();
+							_errorDescriptionDesc.fadeOut();												
+						});	
+				$('#inputPasswordName').on('focus', function(){
+							_error.fadeOut();
+							_errorDescriptionDesc.fadeOut();	
+						});	
+			}
 		
 		
 		
